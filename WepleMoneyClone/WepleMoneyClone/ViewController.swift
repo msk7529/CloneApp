2534a64d0ec6b52eb610d014e12a71fc96453bef
msk7529 <msk7529@naver.com>
2020-12-09 22:28:11 +0900

    프로젝트파일 생성 및 target OS 13.0 설정

---
+++ WepleMoneyClone/WepleMoneyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WepleMoneyClone
+//
+//  Created by kakao on 2020/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
